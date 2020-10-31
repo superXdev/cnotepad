dcf5b38bb9a217045092191e793a86154a74f636
Fikri <fikrizhanking@gmail.com>
2020-10-31 22:14:28 +0700

    Update

--- CNotepad/About.Designer.cs
+++ CNotepad/About.Designer.cs
@@ -72,7 +72,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.InitialImage = null;
             this.pictureBox1.Location = new System.Drawing.Point(23, 26);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(110, 103);
